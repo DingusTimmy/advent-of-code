3b894ef7a7a2f2c9ce0c1759a2f76ec563e0d58f
Tim Healey <timhealeydev@gmail.com>
2021-12-02 23:02:29 +1030

    Add 2021 challenge 01 part 2
    TODO:
    - Add test for sonarSweep2
    - Learn bazel BUILD

--- 2021/01-sonar-sweep.go
+++ 2021/01-sonar-sweep.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -23,18 +24,39 @@ func main() {
 	fmt.Println(scanner.Text())
 	previous := scanner.Text()
 
+	// setup 4 int window
+	var window [4]string
+	window[0] = previous
+	currentLine := 1
+	windowIncreaseCount := 0
+
 	// scan and compare remainder of file
 	for scanner.Scan() {
+		currentLine++
 		current := scanner.Text()
+
 		increased, printString := sonarSweep(previous, current)
 		previous = current
 		if increased {
 			increaseCount++
 		}
 		fmt.Println(printString)
+
+		window[3] = window[2]
+		window[2] = window[1]
+		window[1] = window[0]
+		window[0] = current
+		if currentLine > 3 {
+			windowIncreased, windowPrintString := sonarSweep2(window[0], window[1], window[2], window[3])
+			if windowIncreased {
+				windowIncreaseCount++
+			}
+			fmt.Println(windowPrintString)
+		}
 	}
 
-	fmt.Printf("Total increases: %v", increaseCount)
+	fmt.Printf("Total increases: %v\n", increaseCount)
+	fmt.Printf("Total window increases: %v\n", windowIncreaseCount)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -43,6 +65,50 @@ func main() {
 
 func sonarSweep(previous, current string) (increased bool, printString string) {
 	printString = current
+	increased = false
+
+	intPrevious, err := strconv.Atoi(previous)
+	if err != nil {
+		log.Fatal(err)
+	}
+	intCurrent, err := strconv.Atoi(current)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if intCurrent > intPrevious {
+		printString = printString + " (increased)"
+		increased = true
+	} else if intCurrent < intPrevious {
+		printString = printString + " (decreased)"
+	} else {
+		printString = printString + " (equal)"
+	}
+
+	return increased, printString
+}
+
+func sonarSweep2(a, b, c, d string) (increased bool, printString string) {
+	intA, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	intB, err := strconv.Atoi(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	intC, err := strconv.Atoi(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	intD, err := strconv.Atoi(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	previous := intB + intC + intD
+	current := intA + intB + intC
+	printString = fmt.Sprintf("%v -> %v", previous, current)
+	increased = false
 
 	if current > previous {
 		printString = printString + " (increased)"
